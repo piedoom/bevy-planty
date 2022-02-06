7a73a99bcdda1dadb07a962ffe438a8e5237e69e
doomy <2640792-_doomy@users.noreply.gitlab.com>
2022-02-06 07:37:28 -0600

    better default keybinds

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
-use bevy::{core_pipeline::ClearColor, prelude::*, DefaultPlugins};
+use bevy::{
+    core_pipeline::ClearColor,
+    input::mouse::{MouseMotion, MouseScrollUnit, MouseWheel},
+    prelude::*,
+    DefaultPlugins,
+};
 use bevy_egui::EguiPlugin;
 use bevy_polyline::PolylinePlugin;
 use smooth_bevy_cameras::{
-    controllers::orbit::{OrbitCameraBundle, OrbitCameraController, OrbitCameraPlugin},
+    controllers::orbit::{
+        ControlEvent, OrbitCameraBundle, OrbitCameraController, OrbitCameraPlugin,
+    },
     LookTransformPlugin,
 };
 
@@ -28,7 +35,10 @@ impl Plugin for GamePlugin {
             .add_plugin(ui::UiPlugin)
             .add_plugin(EguiPlugin)
             .add_plugin(LookTransformPlugin)
-            .add_plugin(OrbitCameraPlugin::default())
+            .add_plugin(OrbitCameraPlugin {
+                override_input_system: true,
+            })
+            .add_system(input_map_system)
             .add_startup_system(setup);
 
         #[cfg(target_arch = "wasm32")]
@@ -57,4 +67,60 @@ fn setup(mut commands: Commands) {
         Vec3::new(-10.0, 5.0, -10.0),
         Vec3::new(0., 5., 0.),
     ));
+}
+
+pub fn input_map_system(
+    mut events: EventWriter<ControlEvent>,
+    mut mouse_wheel_reader: EventReader<MouseWheel>,
+    mut mouse_motion_events: EventReader<MouseMotion>,
+    mouse_buttons: Res<Input<MouseButton>>,
+    keyboard: Res<Input<KeyCode>>,
+    controllers: Query<&OrbitCameraController>,
+) {
+    // Can only control one camera at a time.
+    let controller = if let Some(controller) = controllers.iter().next() {
+        controller
+    } else {
+        return;
+    };
+    let OrbitCameraController {
+        enabled,
+        mouse_rotate_sensitivity,
+        mouse_translate_sensitivity,
+        mouse_wheel_zoom_sensitivity,
+        pixels_per_line,
+        ..
+    } = *controller;
+
+    if !enabled {
+        return;
+    }
+
+    let mut cursor_delta = Vec2::ZERO;
+    for event in mouse_motion_events.iter() {
+        cursor_delta += event.delta;
+    }
+
+    if mouse_buttons.pressed(MouseButton::Middle) {
+        if keyboard.pressed(KeyCode::LShift) {
+            //Pan
+            events.send(ControlEvent::TranslateTarget(
+                mouse_translate_sensitivity * cursor_delta,
+            ));
+        } else {
+            // Orbit
+            events.send(ControlEvent::Orbit(mouse_rotate_sensitivity * cursor_delta));
+        }
+    }
+
+    let mut scalar = 1.0;
+    for event in mouse_wheel_reader.iter() {
+        // scale the event magnitude per pixel or per line
+        let scroll_amount = match event.unit {
+            MouseScrollUnit::Line => event.y,
+            MouseScrollUnit::Pixel => event.y / pixels_per_line,
+        };
+        scalar *= 1.0 - scroll_amount * mouse_wheel_zoom_sensitivity;
+    }
+    events.send(ControlEvent::Zoom(scalar));
 }
